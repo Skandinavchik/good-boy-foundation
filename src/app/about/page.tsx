a762beff91a1dba9a90839e66a4077e74231f836
Artem Sokolov <sknd2569@gmail.com>
2026-07-13 13:29:30 +0200

    added contact, about routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react'
+import { Footer } from '@/components/layout/footer'
+
+const AboutPage: FC = () => {
+  return (
+    <div className="flex min-h-screen flex-col justify-between py-8 lg:py-12">
+      <main className="flex-1">
+        {/* Placeholder per user instruction */}
+        <div className="text-lg font-medium text-neutral-800">
+          About Content
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default AboutPage
